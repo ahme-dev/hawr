6f7675b7a9ae5980b9619e6d1e862f7a6c9fa340
Ahmed K. A <ahmadkabdullah@pm.me>
2022-06-16 22:17:49 +0300

    Add more parts to day component and modify header border

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -1,9 +1,15 @@
 import "./Day.css";
+import cloudy from "../images/clouds.png";
 
 function Day() {
   return (
     <div className="Day">
-      <h1>Hello</h1>
+      <img className="Day-img" src={cloudy}></img>
+      <div className="Day-text">
+        <h1>Cloudy</h1>
+        <h3>28*</h3>
+        <p>Sunday 28/6</p>
+      </div>
     </div>
   );
 }

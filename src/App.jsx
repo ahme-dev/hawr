70b8ea0c27c8e615f6e48136ad96109818db63b5
Ahmed K. A <ahmadkabdullah@pm.me>
2022-06-18 21:16:42 +0300

    Redo the ui and remove the popup for now

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,25 @@ import React from "react";
 import "./App.css";
 import Day from "./components/Day";
 import Button from "./components/Button";
-import Popup from "./components/Popup";
 
 import settingsImg from "./images/settings.png";
 import cloudy from "./images/clouds.png";
 
 function App() {
-  let [popup, setPopup] = React.useState("");
-
   return (
     <div className="App">
-      <Popup title="Languages" content="less" visibility={popup}></Popup>
-
       <header>
-        <Day status="Sunny" temp="20*" date="Today 28/6" image={cloudy}></Day>
+        <img src="#"></img>
+        <h1>Hawr</h1>
       </header>
       <main>
         <Day status="Sunny" temp="20*" date="Today 28/6" image={cloudy}></Day>
         <Day status="Sunny" temp="20*" date="Today 28/6" image={cloudy}></Day>
+        <Day status="Sunny" temp="20*" date="Today 28/6" image={cloudy}></Day>
       </main>
       <footer>
-        <Button image={settingsImg} click={() => {}}></Button>
-        <Button image={settingsImg} click={() => {}}></Button>
-        <Button image={settingsImg} click={() => {}}></Button>
+        <Button image={settingsImg}></Button>
+        <Button text="Hello"></Button>
       </footer>
     </div>
   );

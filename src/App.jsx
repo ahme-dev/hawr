9971621585c6bd489b67b1e365fdb82610c3190d
Ahmed K. A <ahmadkabdullah@pm.me>
2022-06-18 21:32:00 +0300

    Modify style and remove button component

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,41 @@ import React from "react";
 
 import "./App.css";
 import Day from "./components/Day";
-import Button from "./components/Button";
 
 import settingsImg from "./images/settings.png";
-import cloudy from "./images/clouds.png";
+import cloudyImg from "./images/clouds.png";
+import iconImg from "./images/icon.png";
 
 function App() {
   return (
     <div className="App">
       <header>
-        <img src="#"></img>
-        <h1>Hawr</h1>
+        <img src={iconImg} className="clickable"></img>
+        <h1 className="clickable">Hawr</h1>
       </header>
       <main>
-        <Day status="Sunny" temp="20*" date="Today 28/6" image={cloudy}></Day>
-        <Day status="Sunny" temp="20*" date="Today 28/6" image={cloudy}></Day>
-        <Day status="Sunny" temp="20*" date="Today 28/6" image={cloudy}></Day>
+        <Day
+          status="Sunny"
+          temp="20*"
+          date="Today 28/6"
+          image={cloudyImg}
+        ></Day>
+        <Day
+          status="Sunny"
+          temp="20*"
+          date="Today 28/6"
+          image={cloudyImg}
+        ></Day>
+        <Day
+          status="Sunny"
+          temp="20*"
+          date="Today 28/6"
+          image={cloudyImg}
+        ></Day>
       </main>
       <footer>
-        <Button image={settingsImg}></Button>
-        <Button text="Hello"></Button>
+        <img src={settingsImg} className="clickable"></img>
+        <h1 className="clickable">Location</h1>
       </footer>
     </div>
   );

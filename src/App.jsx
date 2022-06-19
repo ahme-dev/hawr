3333f6a352d2b1ff022d0fb279293f95cdce8cb9
Ahmed K. A <ahmadkabdullah@pm.me>
2022-06-19 17:09:04 +0300

    Improve interface and change some text to kurdish

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,32 @@ import React from "react";
 
 import "./App.css";
 import Day from "./components/Day";
-
-import settings from "./images/settings.png";
-import cloudy from "./images/clouds.png";
-import icon from "./images/icon.png";
 import apiCall from "./api";
+
+import themeImg from "./images/theme.png";
+import iconImg from "./images/icon.png";
+import sourceImg from "./images/source.png";
+import noneImg from "./images/none.png";
 
 function App() {
   let [weather, setWeather] = React.useState({
     first: {
-      date: "Today",
+      date: "ئەمڕۆ",
       status: "Unknown",
       temprature: "?",
+      image: noneImg,
     },
     second: {
-      date: "Tomorrow",
+      date: "سبەی",
       status: "Unknown",
       temprature: "?",
+      image: noneImg,
     },
     third: {
-      date: "Overmorrow",
+      date: "دووسبەی",
       status: "Unknown",
       temprature: "?",
+      image: noneImg,
     },
   });
 
@@ -33,17 +37,17 @@ function App() {
     setWeather(() => {
       return {
         first: {
-          date: "Today",
+          date: "ئەمڕۆ",
           status: fetched["current"].condition.text,
           temprature: fetched["current"].temp_c,
         },
         second: {
-          date: "Tomorrow",
+          date: "سبەی",
           status: fetched["forecast"]["forecastday"][1]["day"].condition.text,
           temprature: fetched["forecast"]["forecastday"][1]["day"].avgtemp_c,
         },
         third: {
-          date: "Overmorrow",
+          date: "دووسبەی",
           status: fetched["forecast"]["forecastday"][2]["day"].condition.text,
           temprature: fetched["forecast"]["forecastday"][2]["day"].avgtemp_c,
         },
@@ -54,33 +58,38 @@ function App() {
   return (
     <div className="App">
       <header>
-        <img src={icon} className="clickable"></img>
-        <h1 className="clickable">Hawr</h1>
+        <div className="clickable">
+          <h1>هەور</h1>
+          <img src={iconImg}></img>
+        </div>
       </header>
       <main>
         <Day
           status={weather.first.status}
           temp={weather.first.temprature}
           date={weather.first.date}
-          image={cloudy}
+          image={weather.first.image}
         ></Day>
         <Day
           status={weather.second.status}
           temp={weather.second.temprature}
           date={weather.second.date}
-          image={cloudy}
+          image={weather.first.image}
         ></Day>
         <Day
           status={weather.third.status}
           temp={weather.third.temprature}
           date={weather.third.date}
-          image={cloudy}
+          image={weather.first.image}
         ></Day>
       </main>
       <footer>
-        <img src={settings} className="clickable"></img>
+        <div>
+          <img src={sourceImg} className="clickable"></img>
+          <img src={themeImg} className="clickable"></img>
+        </div>
         <h1 onClick={changeWeather} className="clickable">
-          Location
+          شوێن
         </h1>
       </footer>
     </div>
